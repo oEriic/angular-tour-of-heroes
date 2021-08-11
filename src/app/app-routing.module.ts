049c4867d84ca81691d91154739f44693656267d
Eric <o.eric0521@gmail.com>
2021-08-11 15:58:40 -0400

    starting of step 4

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router'
+import { HeroesComponent } from './heroes/heroes.component';
 
-const routes: Routes = [];
+import { CommonModule } from '@angular/common';
+
+const routes: Routes = [
+  { path: 'heroes', component: HeroesComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    CommonModule
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
